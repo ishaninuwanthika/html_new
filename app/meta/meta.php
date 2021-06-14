5fde7efaf5cc6477336edc27b54e87e41d4772d0
ishani <ishani@creativehub.lk>
2021-06-14 21:48:31 +0530

    basic structre one

---
+++ app/meta/meta.php
@@ -0,0 +1,54 @@
+<!-- Meta -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta http-equiv="content-language" content="en-us" />
+<meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+<meta http-equiv="Pragma" content="no-cache" />
+<meta http-equiv="Expires" content="0" />
+
+<meta name="description" content="Turret">
+<meta name="keywords" content="Turret">
+<meta name="author" content="Turret">
+
+
+
+
+
+
+<link rel="apple-touch-icon" sizes="180x180" href="<?php echo URL?>assets/img/favicon/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="<?php echo URL?>assets/img/favicon/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="<?php echo URL?>assets/img/favicon/favicon-16x16.png">
+<link rel="manifest" href="<?php echo URL?>assets/img/favicon/site.webmanifest">
+<link rel="mask-icon" href="<?php echo URL?>assets/img/favicon/safari-pinned-tab.svg" color="#5bbad5">
+<meta name="msapplication-TileColor" content="#da532c">
+<meta name="theme-color" content="#ffffff">
+
+
+
+<!-- <link rel="apple-touch-icon" sizes="180x180" href="<?php echo URL?>assets/img/favicon/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="<?php echo URL?>assets/img/favicon/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="<?php echo URL?>assets/img/favicon/favicon-16x16.png">
+<link rel="manifest" href="<?php echo URL?>assets/img/favicon/site.webmanifest">
+<link rel="mask-icon" href="<?php echo URL?>assets/img/favicon/safari-pinned-tab.svg" color="#5bbad5">
+<meta name="msapplication-TileColor" content="#da532c">
+<meta name="theme-color" content="#ffffff"> -->
+
+
+
+
+
+<!-- <link rel="apple-touch-icon" sizes="180x180" href="<?php echo URL?>assets/img/favicon/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="<?php echo URL?>assets/img/favicon/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="<?php echo URL?>assets/img/favicon/favicon-16x16.png">
+<link rel="manifest" href="<?php echo URL?>assets/img/favicon/site.webmanifest">
+<link rel="mask-icon" href="<?php echo URL?>assets/img/favicon/safari-pinned-tab.svg" color="#ffffff">
+<meta name="msapplication-TileColor" content="#da532c">
+<meta name="theme-color" content="#ffffff"> -->
+
+
+
+
+
